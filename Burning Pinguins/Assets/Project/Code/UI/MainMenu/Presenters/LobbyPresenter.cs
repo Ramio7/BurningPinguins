db0b0b4c1eeea215d1b34fdc29eb6921dc88ef7f
Ramio7 <Dunadan2007@yandex.ru>
2023-09-04 18:03:39 +0300

    UI build in progress

--- Burning Pinguins/Assets/Project/Code/UI/MainMenu/Presenters/LobbyPresenter.cs
+++ Burning Pinguins/Assets/Project/Code/UI/MainMenu/Presenters/LobbyPresenter.cs
@@ -1,8 +1,13 @@
 using Photon.Pun;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class LobbyPresenter : MonoBehaviourPunCallbacks, IUiWindow
 {
+    [SerializeField] private Button _createNewGameButton;
+    [SerializeField] private Button _joinFriendButton;
+    [SerializeField] private Button _backToMainMenuButton;
+
     public static Canvas Canvas { get; private set; }
 
     public override void OnEnable()
@@ -19,11 +24,31 @@ public class LobbyPresenter : MonoBehaviourPunCallbacks, IUiWindow
 
     private void SubscribeButtons()
     {
-        
+        _createNewGameButton.onClick.AddListener(SwitchToCreateNewGameWindow);
+        _joinFriendButton.onClick.AddListener(SwitchToGameChoiceWindow);
+        _backToMainMenuButton.onClick.AddListener(SwitchToMainMenu);
     }
 
     private void UnsubscribeButtons()
     {
-        
+        _createNewGameButton.onClick.RemoveListener(SwitchToCreateNewGameWindow);
+        _joinFriendButton.onClick.RemoveListener(SwitchToGameChoiceWindow);
+        _backToMainMenuButton.onClick.RemoveListener(SwitchToMainMenu);
+    }
+
+    private void SwitchToCreateNewGameWindow()
+    {
+
+    }
+
+    private void SwitchToGameChoiceWindow()
+    {
+
+    }
+
+    private void SwitchToMainMenu()
+    {
+        Canvas.enabled = false;
+        MainMenuPresenter.Canvas.enabled = true;
     }
 }
